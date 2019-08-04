4a655d2e367078ac4d834f7b34a2fa5a98125c38
Love <love.wick93@gmail.com>
2019-08-04 21:01:41 +0200

    Inital set up

---
+++ UmbracoQuiz/UmbracoQuiz.Core/Controllers/GameRoomController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.PublishedModels;
+
+namespace UmbracoQuiz.Core.Controllers
+{
+    public class GameRoomController : SurfaceController
+    {
+        [HttpGet]
+        public ActionResult EnterRoom(int nodeId, string roomCode)
+        {
+            var room = (GameRoomPage)Umbraco.Content(nodeId);
+            if (roomCode == room.RoomCode)
+            {
+                return RedirectToUmbracoPage(room);
+            }
+
+            ModelState.AddModelError(nodeId.ToString(), "Felaktig rumkod");
+            return CurrentUmbracoPage();
+        }
+    }
+}
